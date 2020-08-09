85676db5c03a972d1661064134e01248e517ba83
Joshua Alzate <joshua@hlbb.hongleong.com.my>
2020-08-10 00:53:02 +0800

    Map getAllVideo to return Video Model Collection

--- src/app/shared/service/video.service.ts
+++ src/app/shared/service/video.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { Video } from 'src/app/shared/model/Video.model';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,13 @@ export class VideoService {
   private apiURL: string = environment.apiURL;
   constructor(private http: HttpClient) { }
 
-  public async getAllVideo(): Promise<number[]> {
+  public async getAllVideo(): Promise<Video[]> {
     const serviceURL: string = this.apiURL + 'getAllVideo';
-    const result = this.http.get(serviceURL).toPromise();
-    return <number[]><unknown>result;
+
+    const videoData: any = await this.http.get(serviceURL).toPromise();
+    return videoData.reduce((prev, curr) => {
+      prev.push(new Video(curr.id, curr.name, this.apiURL + 'streamVideo?id=' + curr.id));
+      return prev;
+    }, [])
   }
 }
