09daafc2540340a36b26ac7ed97147ed1a8e10c6
Joshua Alzate <joshua@hlbb.hongleong.com.my>
2020-08-10 00:52:35 +0800

    Add Video Model

---
+++ src/app/shared/model/Video.model.ts
@@ -0,0 +1,17 @@
+
+export class Video {
+    public id: number;
+    public name: string;
+    public path: string;
+    public viewCount: number;
+    public viewHistory: { leftAt: number }[];
+    public hasBeenLoaded: boolean;
+
+    constructor(id: number, name: string, path: string) {
+        this.id = id;
+        this.name = name;
+        this.path = path;
+        this.viewCount = 0;
+        this.viewHistory = [];
+    }
+}
