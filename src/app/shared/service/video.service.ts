1ed71db032435ffe3f330c62f51f7b81463b7d81
Joshua Alzate <joshua@hlbb.hongleong.com.my>
2020-08-11 00:49:02 +0800

    Change viewCount to Observable<number> stream type

--- src/app/shared/service/video.service.ts
+++ src/app/shared/service/video.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { HttpHeaders, HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { Video } from 'src/app/shared/model/Video.model';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -15,8 +17,27 @@ export class VideoService {
 
     const videoData: any = await this.http.get(serviceURL).toPromise();
     return videoData.reduce((prev, curr) => {
-      prev.push(new Video(curr.id, curr.name, this.apiURL + 'streamVideo?id=' + curr.id));
+      const video = new Video(curr.id, curr.name, this.apiURL + 'streamVideo?id=' + curr.id);
+      video.viewCount = this.getVideoViewCount(video.id);
+      prev.push(video);
       return prev;
     }, [])
+  }
+
+  public async viewVideo(id: number): Promise<void> {
+    const serviceURL: string = this.apiURL + 'viewVideo';
+    const videoData: any = await this.http.put(serviceURL, {
+      id: id
+    }).toPromise();
+    return videoData;
+  }
+
+  public getVideoViewCount(id: number): Observable<number> {
+    const serviceURL: string = this.apiURL + 'getVideoViewCount';
+    return this.http.get(serviceURL, {
+      params: {
+        id: id.toString()
+      }
+    }).pipe(map((data) => <number>data['viewCount']));
   }
 }
