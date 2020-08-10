a6607b5fafd001784a367fbe7e94a690c1e5fd14
Joshua Alzate <joshua@hlbb.hongleong.com.my>
2020-08-11 00:49:21 +0800

    Change viewCount to Observable<number> stream type

--- src/app/shared/model/Video.model.ts
+++ src/app/shared/model/Video.model.ts
@@ -1,17 +1,18 @@
+import { Observable } from 'rxjs';
 
 export class Video {
     public id: number;
     public name: string;
     public path: string;
-    public viewCount: number;
+    public viewCount: Observable<number>;
     public viewHistory: { leftAt: number }[];
     public hasBeenLoaded: boolean;
+    public hasBeenViewed: boolean;
 
     constructor(id: number, name: string, path: string) {
         this.id = id;
         this.name = name;
         this.path = path;
-        this.viewCount = 0;
         this.viewHistory = [];
     }
 
