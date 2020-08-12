3eb108f6a4e7667a35adfddf390625e9218bd19f
Joshua Alzate <joshua@hlbb.hongleong.com.my>
2020-08-13 00:35:59 +0800

    Add Complete property to view history

--- src/app/shared/model/Video.model.ts
+++ src/app/shared/model/Video.model.ts
@@ -5,9 +5,10 @@ export class Video {
     public name: string;
     public path: string;
     public viewCount: Observable<number>;
-    public viewHistory: { leftAt: number }[];
+    public viewHistory: { leftAt: number, complete: boolean }[];
     public hasBeenLoaded: boolean;
     public hasBeenViewed: boolean;
+    public hasBeenEnded: boolean;
 
     constructor(id: number, name: string, path: string) {
         this.id = id;
@@ -19,5 +20,13 @@ export class Video {
     getVideoPlayer() {
         const selector = this.name + this.id;
         return document.getElementById(selector);
+    }
+
+    applyVideoEndedListener() {
+        const videoPlayer: any = this.getVideoPlayer();
+        videoPlayer.onended = () => {
+            this.hasBeenEnded = true;
+            videoPlayer.play();
+        }
     }
 }
